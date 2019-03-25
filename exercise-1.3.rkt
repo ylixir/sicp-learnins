28bf45caaf17ca879c0c6ca400120061b856d8da
Jon Allen <jon@ylixir.io>
2019-03-25 22:59:06 +0100

    1.2, 1.3

---
+++ exercise-1.3.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+(define (1-3 a b c) (
+   cond ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
+        ((and (<= b a) (<= b c)) (+ (* a a ) (* c c)))
+        (else (+ (* a a) (* b b)))
+        )
+  )
+  (1-3 2 2 3)
